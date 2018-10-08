5a31f863ed0e0b775a1e71639fa60c2e91422898
Mohammad Yazdani <mohammad.yazdani@optimity.onmicrosoft.com>
2018-10-08 16:23:16 -0700

    initial Dicee app commit

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Mohammad Yazdani on 9/30/18.
+//  Copyright © 2018 MYazdani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDiceImages()
+    }
+
+    
+    /**
+     * Executes when the Roll Button is pressed in the screen
+     **/
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    /**
+     * Execute when the user shakes the phone
+     **/
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    /**
+     * Update the 2 dice images randomly 
+     **/
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        
+        print("\(randomDiceIndex1), \(randomDiceIndex2)")
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+}
+
