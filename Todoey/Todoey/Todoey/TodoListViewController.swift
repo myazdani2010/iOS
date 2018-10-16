a1d1054ace59cdbb78d2d64af5a57d6995af05d2
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-10-16 12:57:17 -0700

    Todoey: add items to the table view

--- Todoey/Todoey/Todoey/TodoListViewController.swift
+++ Todoey/Todoey/Todoey/TodoListViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+//UITableViewController manages the table and takes care of delegate as well
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Milk", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    //cellForRowAt : used to display date in the cell at the given row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) //"ToDoItemCell" is the identifirer that is given in the attribute inspecture -> Table view Cell -> identifier
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //numberOfRowInSection: to create the required number of cells in the table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
 
 
