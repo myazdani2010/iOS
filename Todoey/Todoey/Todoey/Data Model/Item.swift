03a0b7fb031232e587fc92948f0a98b483241198
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-10-22 12:06:52 -0700

    Todoey: fix the comments

--- Todoey/Todoey/Todoey/Data Model/Item.swift
+++ Todoey/Todoey/Todoey/Data Model/Item.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+import RealmSwift
+
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    //backward relationship
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+}
