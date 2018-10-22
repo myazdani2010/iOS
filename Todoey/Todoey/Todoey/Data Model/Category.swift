6b74a1c423c74fe38f6ce88b461523433c27a8b1
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-10-22 12:04:41 -0700

    Todoey: replace CoreData model with Realm's data model classes

---
+++ Todoey/Todoey/Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Mohammad Yazdani on 10/22/18.
+//  Copyright © 2018 MYazdani. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mohammad Yazdani on 10/22/18.
+//  Copyright © 2018 MYazdani. All rights reserved.
+//
+
+import Foundation
